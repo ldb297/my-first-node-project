30bc1a05bf64e9f00c674879ea0fa46cfe52d4e3
Leonidas <ldbrooks297@gmail.com>
2021-02-01 11:38:30 -0800

    moment practice

--- main.js
+++ main.js
@@ -1,15 +1,31 @@
-const myModule = require("./myModule");
-const myOtherModule = require("./myOtherModule");
-const myThirdModule = require("./myThirdModule");
-console.log("hello world");
+// const myModule = require("./myModule");
+// const myOtherModule = require("./myOtherModule");
+// const myThirdModule = require("./myThirdModule");
+// console.log("hello world");
 
-Hello = (name)=>{
-    console.log(`Hello, ${name}`);
-}
+// Hello = (name)=>{
+//     console.log(`Hello, ${name}`);
+// }
 
-Hello("Leo");
+// Hello("Leo");
 
-console.log(myModule.add(2,2));
-console.log(myModule.subtract(2,1));
-myOtherModule.welcomeToNode();
-console.log(myThirdModule.mult(3,3));
+// console.log(myModule.add(2,2));
+// console.log(myModule.subtract(2,1));
+// myOtherModule.welcomeToNode();
+// console.log(myThirdModule.mult(3,3));
+
+// const fs = require("fs");
+const moment = require("moment");
+moment.suppressDeprecationWarnings = true;
+
+// fs.readFile("nfl.txt", "utf8", function(error, data){
+//     if (error){
+//         console.log(`You have a problem reading this file`);
+//     } else {
+//         console.log(data);
+//     }
+// });
+
+let myDate = moment("2021-2-01");
+let addOneWeek = myDate.add(1, "week");
+console.log(myDate.format("MM/DD/YYYY"));
