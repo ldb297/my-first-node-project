5b7989788037ca2632411c22c14358e805b9e330
Leonidas <ldbrooks297@gmail.com>
2021-02-01 10:50:56 -0800

    add modules

--- main.js
+++ main.js
@@ -1,7 +1,15 @@
+const myModule = require("./myModule");
+const myOtherModule = require("./myOtherModule");
+const myThirdModule = require("./myThirdModule");
 console.log("hello world");
 
 Hello = (name)=>{
-    console.log(`Hello, ${name}`)
+    console.log(`Hello, ${name}`);
 }
 
-Hello("Leo")
+Hello("Leo");
+
+console.log(myModule.add(2,2));
+console.log(myModule.subtract(2,1));
+myOtherModule.welcomeToNode();
+console.log(myThirdModule.mult(3,3));
---
+++ myOtherModule.js
@@ -0,0 +1,3 @@
+module.exports.welcomeToNode = function(){
+    console.log(`Welcome To Node`);
+}
